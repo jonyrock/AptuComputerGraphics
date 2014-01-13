9db0e9eb9deb4c430d1406e563cea3396c6ac7e4
jonyrock <ilms@live.ru>
2014-01-14 01:12:11 +0400

    release make

--- task2/nbproject/Makefile-variables.mk
+++ task2/nbproject/Makefile-variables.mk
@@ -6,14 +6,6 @@
 CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
-# Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=task2
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/task2
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=task2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/task2.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
@@ -22,6 +14,14 @@ CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/task2
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=task2.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/task2.tar
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=task2
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/task2
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=task2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/task2.tar
 #
 # include compiler specific variables
 #
