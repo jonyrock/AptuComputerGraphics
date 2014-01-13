07497088a6b527a84184077107c4fe9cdefa1dc2
jonyrock <ilms@live.ru>
2014-01-13 19:55:00 +0400

    filters navigation

--- task2/include/textureNavigation.h
+++ task2/include/textureNavigation.h
@@ -3,16 +3,21 @@
 #include <GL/glfw.h>
 #include <GL/glew.h>
 
+#include <iostream>
+using namespace std;
+
 class TextureNavigation {
     GLuint _textureID;
     GLuint _textureScaleId;
     float _ts;
+    int _filterType;
 
 public:
 
     TextureNavigation(GLuint textureID, GLuint textureScaleId)
     : _textureID(textureID), _textureScaleId(textureScaleId) {
         _ts = 1;
+        _filterType = 1;
     }
 
     inline void windowsIterate() {
@@ -20,8 +25,13 @@ public:
             _ts -= 0.1;
         if (glfwGetKey(GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS)
             _ts += 0.1;
-
         glUniform1f(_textureScaleId, _ts);
+
+        for (int i = 1; i <= 6; i++) {
+            if (glfwGetKey('0' + i) == GLFW_PRESS)
+                _filterType = i;
+        }
+
         setTexturesParam();
     }
 
@@ -29,17 +39,46 @@ private:
 
     inline void setTexturesParam() {
 
+
         glBindTexture(GL_TEXTURE_2D, _textureID);
 
-        // Poor filtering, or ...
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        //    glGenerateMipmap(GL_TEXTURE_2D);
+        if (_filterType == 1) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        }
+
+        if (_filterType == 2) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        }
+
+        if (_filterType == 3) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+        }
+        
+        if (_filterType == 4) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+        }
+        
+        if (_filterType == 5) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+        }
+        
+        if (_filterType == 6) {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        }
+
+
+        if (_filterType > 2) {
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
 
     }
 
